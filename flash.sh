543bea86002e72bb80d9e29586c38a10b56c852b
Thomas Roth <code@stacksmashing.net>
2020-11-24 14:15:45 +0100

    Use stlink, fix usage from other directories

--- flash.sh
+++ flash.sh
@@ -12,7 +12,7 @@ fi
 
 echo "Loading image into RAM..."
 
-openocd -f ${DIR}/adapter_config.cfg -c "load_image $1 0x24000000" -f ${DIR}/post_load.cfg  &>/dev/null
+openocd -f ${DIR}/adapter_config.cfg -c "load_image $1 0x24000000" -c "program ${DIR}/build/gw_base.elf; reset; exit"
 
 echo "Please wait til the screen blinks once per second."
 echo "(Rapid blinking means an error occured)"
